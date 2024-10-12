9ab2907860c645feffc57ca7a2b599a6b4272377
SalazarMalagon <andressalazar062858@gmail.com>
2024-10-12 13:59:56 -0500

    actividad en clase

--- Class_work/Class12-10-2024/Clase12-10-2024.py
+++ Class_work/Class12-10-2024/Clase12-10-2024.py
@@ -3,11 +3,22 @@
 pyenv-venv install 3.11.1 EntornoREDES → usado para installar el entorno
 pyenv-venv activate EntornoREDES → usado para activar el entorno
 
+fastapi dev main.py → sirve para ejecutar la api
 """
 
 from fastapi import FastAPI
 
 api = FastAPI()
 
-#@api.get("/Inicio") #Define a GET endpont
+@api.get("/") #Define a GET endpont
 
+def healthcheck():
+    return {"status":"I'am allive"}
+
+@api.get("/hello_UD")
+
+def hello_UD():
+    message = {"date":"2021-09-01",
+               "message":"Hello UD students",
+               "class":"Computer Networking"}
+    return message
