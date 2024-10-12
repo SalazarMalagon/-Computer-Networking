f29e9207374b194f38fd7b183bf69183ee62b0ee
SalazarMalagon <andressalazar062858@gmail.com>
2024-10-12 12:56:53 -0500

    Create Clase12-10-2024.py

---
+++ Class_work/Clase12-10-2024.py
@@ -0,0 +1,6 @@
+"""Usando entorno virtual de python 3.11.1 llamada EntornoREDES
+
+pyenv-venv install 3.11.1 EntornoREDES → usado para installar el entorno
+pyenv-venv activate EntornoREDES → usado para activar el entorno
+
+"""
